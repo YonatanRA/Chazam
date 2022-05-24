b1c312b6c6d8d0f6c8084d493c0eb259021af5a9
YonatanRA <yeho@mail.com>
2022-05-24 17:16:21 +0200

    libreria chazam

---
+++ data/chazam/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+# import os, sys
+
+
+def parse_requirements(requirements):
+    # load from requirements.txt
+    with open(requirements) as f:
+        lines = [l for l in f]
+        # remove spaces
+        stripped = map((lambda x: x.strip()), lines)
+        # remove comments
+        nocomments = filter((lambda x: not x.startswith('#')), stripped)
+        # remove empty lines
+        reqs = filter((lambda x: x), nocomments)
+        return reqs
+
+PACKAGE_NAME = 'Chazam'
+PACKAGE_VERSION = '0.1'
+SUMMARY = 'Chazam: Audio Fingerprinting in Python'
+DESCRIPTION = """
+Audio fingerprinting and recognition algorithm implemented in Python
+
+See the explanation here: 
+
+`http://willdrevo.com/fingerprinting-and-audio-recognition-with-python/`__
+
+Chazam can memorize recorded audio by listening to it once and fingerprinting 
+it. Then by playing a song and recording microphone input or on disk file, 
+Chazam attempts to match the audio against the fingerprints held in the 
+database, returning the song or recording being played.
+
+__ http://willdrevo.com/fingerprinting-and-audio-recognition-with-python/
+"""
+REQUIREMENTS = parse_requirements('requirements.txt')
+
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    description=SUMMARY,
+    long_description=DESCRIPTION,
+    author='',
+    author_email='',
+    maintainer='',
+    maintainer_email='',
+    url='',
+    license='MIT License',
+    include_package_data=True,
+    packages=find_packages(),
+    platforms=['Unix'],
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords="python, audio, fingerprinting, music, numpy, landmark",
+)
