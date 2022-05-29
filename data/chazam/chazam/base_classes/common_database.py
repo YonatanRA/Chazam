5300f11c699cf4bb9d2abf1bea7fbad2dfe9cfd3
YonatanRA <yeho@mail.com>
2022-05-29 18:06:24 +0200

    common_database

--- data/chazam/chazam/base_classes/common_database.py
+++ data/chazam/chazam/base_classes/common_database.py
@@ -13,32 +13,32 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
         super().__init__()
 
     def before_fork(self) -> None:
-        """
+        '''
         Called before the database instance is given to the new process
-        """
+        '''
         pass
 
     def after_fork(self) -> None:
-        """
+        '''
         Called after the database instance has been given to the new process
 
         This will be called in the new process.
-        """
+        '''
         pass
 
     def setup(self) -> None:
-        """
+        '''
         Called on creation or shortly afterwards.
-        """
+        '''
         with self.cursor() as cur:
             cur.execute(self.CREATE_SONGS_TABLE)
             cur.execute(self.CREATE_FINGERPRINTS_TABLE)
             cur.execute(self.DELETE_UNFINGERPRINTED)
 
     def empty(self) -> None:
-        """
+        '''
         Called when the database should be cleared of all data.
-        """
+        '''
         with self.cursor() as cur:
             cur.execute(self.DROP_FINGERPRINTS)
             cur.execute(self.DROP_SONGS)
@@ -46,19 +46,19 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
         self.setup()
 
     def delete_unfingerprinted_songs(self) -> None:
-        """
+        '''
         Called to remove any song entries that do not have any fingerprints
         associated with them.
-        """
+        '''
         with self.cursor() as cur:
             cur.execute(self.DELETE_UNFINGERPRINTED)
 
     def get_num_songs(self) -> int:
-        """
+        '''
         Returns the song's count stored.
 
         :return: the amount of songs in the database.
-        """
+        '''
         with self.cursor(buffered=True) as cur:
             cur.execute(self.SELECT_UNIQUE_SONG_IDS)
             count = cur.fetchone()[0] if cur.rowcount != 0 else 0
@@ -66,11 +66,11 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
         return count
 
     def get_num_fingerprints(self) -> int:
-        """
+        '''
         Returns the fingerprints' count stored.
 
         :return: the number of fingerprints in the database.
-        """
+        '''
         with self.cursor(buffered=True) as cur:
             cur.execute(self.SELECT_NUM_FINGERPRINTS)
             count = cur.fetchone()[0] if cur.rowcount != 0 else 0
@@ -78,49 +78,49 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
         return count
 
     def set_song_fingerprinted(self, song_id):
-        """
+        '''
         Sets a specific song as having all fingerprints in the database.
 
         :param song_id: song identifier.
-        """
+        '''
         with self.cursor() as cur:
             cur.execute(self.UPDATE_SONG_FINGERPRINTED, (song_id,))
 
     def get_songs(self) -> List[Dict[str, str]]:
-        """
+        '''
         Returns all fully fingerprinted songs in the database
 
         :return: a dictionary with the songs info.
-        """
+        '''
         with self.cursor(dictionary=True) as cur:
             cur.execute(self.SELECT_SONGS)
             return list(cur)
 
     def get_song_by_id(self, song_id: int) -> Dict[str, str]:
-        """
+        '''
         Brings the song info from the database.
 
         :param song_id: song identifier.
         :return: a song by its identifier. Result must be a Dictionary.
-        """
+        '''
         with self.cursor(dictionary=True) as cur:
             cur.execute(self.SELECT_SONG, (song_id,))
             return cur.fetchone()
 
     def insert(self, fingerprint: str, song_id: int, offset: int):
-        """
+        '''
         Inserts a single fingerprint into the database.
 
         :param fingerprint: Part of a sha1 hash, in hexadecimal format
         :param song_id: Song identifier this fingerprint is off
         :param offset: The offset this fingerprint is from.
-        """
+        '''
         with self.cursor() as cur:
             cur.execute(self.INSERT_FINGERPRINT, (fingerprint, song_id, offset))
 
     @abc.abstractmethod
     def insert_song(self, song_name: str, file_hash: str, total_hashes: int) -> int:
-        """
+        '''
         Inserts a song name into the database, returns the new
         identifier of the song.
 
@@ -128,17 +128,17 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
         :param file_hash: Hash from the fingerprinted file.
         :param total_hashes: amount of hashes to be inserted on fingerprint table.
         :return: the inserted id.
-        """
+        '''
         pass
 
     def query(self, fingerprint: str = None) -> List[Tuple]:
-        """
+        '''
         Returns all matching fingerprint entries associated with
         the given hash as parameter, if None is passed it returns all entries.
 
         :param fingerprint: part of a sha1 hash, in hexadecimal format
         :return: a list of fingerprint records stored in the db.
-        """
+        '''
         with self.cursor() as cur:
             if fingerprint:
                 cur.execute(self.SELECT, (fingerprint,))
@@ -147,15 +147,15 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
             return list(cur)
 
     def get_iterable_kv_pairs(self) -> List[Tuple]:
-        """
+        '''
         Returns all fingerprints in the database.
 
         :return: a list containing all fingerprints stored in the db.
-        """
+        '''
         return self.query(None)
 
     def insert_hashes(self, song_id: int, hashes: List[Tuple[str, int]], batch_size: int = 1000) -> None:
-        """
+        '''
         Insert a multitude of fingerprints.
 
         :param song_id: Song identifier the fingerprints belong to
@@ -163,7 +163,7 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
             - hash: Part of a sha1 hash, in hexadecimal format
             - offset: Offset this hash was created from/at.
         :param batch_size: insert batches.
-        """
+        '''
         values = [(song_id, hsh, int(offset)) for hsh, offset in hashes]
 
         with self.cursor() as cur:
@@ -172,7 +172,7 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
 
     def return_matches(self, hashes: List[Tuple[str, int]],
                        batch_size: int = 1000) -> Tuple[List[Tuple[int, int]], Dict[int, int]]:
-        """
+        '''
         Searches the database for pairs of (hash, offset) values.
 
         :param hashes: A sequence of tuples in the format (hash, offset)
@@ -184,7 +184,7 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
         duplicated hashes) in each song.
             - song id: Song identifier
             - offset_difference: (database_offset - sampled_offset)
-        """
+        '''
         # Create a dictionary of hash => offset pairs for later lookups
         mapper = {}
         for hsh, offset in hashes:
@@ -218,12 +218,12 @@ class CommonDatabase(BaseDatabase, metaclass=abc.ABCMeta):
             return results, dedup_hashes
 
     def delete_songs_by_id(self, song_ids: List[int], batch_size: int = 1000) -> None:
-        """
+        '''
         Given a list of song ids it deletes all songs specified and their corresponding fingerprints.
 
         :param song_ids: song ids to be deleted from the database.
         :param batch_size: number of query's batches.
-        """
+        '''
         with self.cursor() as cur:
             for index in range(0, len(song_ids), batch_size):
                 # Create our IN part of the query
