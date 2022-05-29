23791581508859c092e245cdd44917ebb15f8148
YonatanRA <yeho@mail.com>
2022-05-29 03:36:08 +0200

    recognize

--- data/chazam/chazam/recognize.py
+++ data/chazam/chazam/recognize.py
@@ -6,40 +6,38 @@ import time
 
 class BaseRecognizer():
 
-	def __init__(self, chazam):
-		self.chazam = chazam
-		self.Fs = fingerprint.DEFAULT_FS
+    def __init__(self, chazam):
+        self.chazam = chazam
+        self.Fs = fingerprint.DEFAULT_FS
 
-	def _recognize(self, *data):
-		matches = []
-		for d in data:
-			matches.extend(self.chazam.find_matches(d, Fs=self.Fs))
+    def _recognize(self, *data):
+        matches = []
+        for d in data:
+            matches.extend(self.chazam.find_matches(d, Fs=self.Fs))
 
-		return self.chazam.align_matches(matches)
+        return self.chazam.align_matches(matches)
 
-	def recognize(self):
-		pass  # base class does nothing
+    def recognize(self):
+        pass  # base class does nothing
 
 
 class FileRecognizer(BaseRecognizer):
-	def __init__(self, chazam):
-		super(FileRecognizer, self).__init__(chazam)
+    def __init__(self, chazam):
+        super(FileRecognizer, self).__init__(chazam)
 
-	def recognize_file(self, filename):
-		frames, self.Fs, file_hash = decoder.read(filename, self.chazam.limit)
+    def recognize_file(self, filename):
+        frames, self.Fs, file_hash = decoder.read(filename, self.chazam.limit)
 
-		t = time.time()
-		match = self._recognize(*frames)
-		t = time.time() - t
+        t = time.time()
+        match = self._recognize(*frames)
+        t = time.time() - t
 
-		if match:
-			match['match_time'] = t
+        if match:
+            match['match_time'] = t
 
-		return match
+        return match
 
-	def recognize(self, filename):
-		return self.recognize_file(filename)
+    def recognize(self, filename):
+        return self.recognize_file(filename)
 
 
-class NoRecordingError(Exception):
-	pass
