4481a4fd3fdc4b93ac1a511e2805597590d59160
YonatanRA <yeho@mail.com>
2022-05-29 03:44:37 +0200

    database

--- data/chazam/build/lib/chazam/decoder.py
+++ data/chazam/build/lib/chazam/decoder.py
@@ -7,14 +7,14 @@ import wavio
 from hashlib import sha1
 
 def unique_hash(filepath, blocksize=2**20):
-    """ Small function to generate a hash to uniquely generate
+    ''' Small function to generate a hash to uniquely generate
     a file. Inspired by MD5 version here:
     http://stackoverflow.com/a/1131255/712997
 
     Works with large files. 
-    """
+    '''
     s = sha1()
-    with open(filepath , "rb") as f:
+    with open(filepath , 'rb') as f:
         while True:
             buf = f.read(blocksize)
             if not buf:
@@ -24,18 +24,18 @@ def unique_hash(filepath, blocksize=2**20):
 
 
 def find_files(path, extensions):
-    # Allow both with ".mp3" and without "mp3" to be used for extensions
-    extensions = [e.replace(".", "") for e in extensions]
+    # Allow both with '.mp3' and without 'mp3' to be used for extensions
+    extensions = [e.replace('.', '') for e in extensions]
 
     for dirpath, dirnames, files in os.walk(path):
         for extension in extensions:
-            for f in fnmatch.filter(files, "*.%s" % extension):
+            for f in fnmatch.filter(files, '*.%s' % extension):
                 p = os.path.join(dirpath, f)
                 yield (p, extension)
 
 
 def read(filename, limit=None):
-    """
+    '''
     Reads any file supported by pydub (ffmpeg) and returns the data contained
     within. If file reading fails due to input being a 24-bit wav file,
     wavio is used as a backup.
@@ -45,7 +45,7 @@ def read(filename, limit=None):
     seconds from the start of the file.
 
     returns: (channels, samplerate)
-    """
+    '''
     # pydub does not support 24-bit wav files, use wavio when this occurs
     try:
         audiofile = AudioSegment.from_file(filename)
@@ -77,8 +77,8 @@ def read(filename, limit=None):
 
 
 def path_to_songname(path):
-    """
+    '''
     Extracts song name from a filepath. Used to identify which songs
     have already been fingerprinted on disk.
-    """
+    '''
     return os.path.splitext(os.path.basename(path))[0]
