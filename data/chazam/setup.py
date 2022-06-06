a97d2afb3b5a0a6bd1d21f3aab73076ef306b15b
YonatanRA <yeho@mail.com>
2022-06-06 17:31:12 +0200

    setup

--- data/chazam/setup.py
+++ data/chazam/setup.py
@@ -2,20 +2,20 @@ from setuptools import find_packages, setup
 
 
 def parse_requirements(requirements):
-    # load from requirements.txt
+    # carga el archivo requirements.txt
     with open(requirements) as file:
         lines = [line for line in file]
-        # remove spaces
+        # quita espacios
         stripped = list(map((lambda x: x.strip()), lines))
-        # remove comments
+        # quita comentarios
         nocomments = list(filter((lambda x: not x.startswith('#')), stripped))
-        # remove empty lines
+        # quita lineas en blanco
         reqs = list(filter((lambda x: x), nocomments))
         return reqs
 
 
 if __name__ == '__main__':
-
+    # setup de instalacion
     setup(
         name='Chazam',
         version='0.1.2',
