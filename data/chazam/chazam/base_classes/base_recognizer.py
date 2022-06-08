27ed50624952760913966c3ccd0890db74338ef7
YonatanRA <yeho@mail.com>
2022-06-08 11:56:08 +0200

    base_recognizer

--- data/chazam/chazam/base_classes/base_recognizer.py
+++ data/chazam/chazam/base_classes/base_recognizer.py
@@ -3,8 +3,8 @@ from time import time
 from typing import Dict, List, Tuple
 
 import numpy as np
-
 from chazam.config.settings import DEFAULT_FS
+
 
 class BaseRecognizer(object, metaclass=abc.ABCMeta):
     def __init__(self, chazam):
@@ -13,20 +13,20 @@ class BaseRecognizer(object, metaclass=abc.ABCMeta):
 
     def _recognize(self, *data) -> Tuple[List[Dict[str, any]], int, int, int]:
         fingerprint_times = []
-        hashes = set()  # to remove possible duplicated fingerprints we built a set.
+        hashes = set()  # para borra posibles fingerprints duplicados.
         for channel in data:
             fingerprints, fingerprint_time = self.chazam.generate_fingerprints(channel, Fs=self.Fs)
             fingerprint_times.append(fingerprint_time)
             hashes |= set(fingerprints)
 
-        matches, dedup_hashes, query_time = self.chazam.find_matches(hashes)
+        matches, d_hashes, query_time = self.chazam.find_matches(hashes)
 
         t = time()
-        final_results = self.chazam.align_matches(matches, dedup_hashes, len(hashes))
+        final_results = self.chazam.align_matches(matches, d_hashes, len(hashes))
         align_time = time() - t
 
         return final_results, np.sum(fingerprint_times), query_time, align_time
 
     @abc.abstractmethod
     def recognize(self) -> Dict[str, any]:
-        pass  # base class does nothing
+        pass  # base, no hace nada
